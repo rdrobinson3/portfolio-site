3c367c09f2d9a5da23a638d8ed113012c64c17fc
Trey Robinson <trobinson@captechconsulting.com>
2013-12-17 19:51:43 -0500

    Added map. About page. Fixed some unclosed tags. Switched to webstorm and added idea directory to gitignore.

--- js/site.js
+++ js/site.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
+
          $('.carousel').carousel({
              interval: 5000
          })
-    });    
+
+         
+		var yourStartLatLng = new google.maps.LatLng(59.3426606750, 18.0736160278);
+		$('#map_canvas').gmap({'center': yourStartLatLng});
+
+ });    
+
