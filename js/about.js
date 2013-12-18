52893fade7725a2aa7b2684cf130e76dd628c3a1
Trey Robinson <trobinson@captechconsulting.com>
2013-12-18 10:06:16 -0500

    Broke up js by page. Made carousel responsive but it is still using header from About as the image.

--- js/about.js
+++ js/about.js
@@ -1,9 +1,5 @@
 $(document).ready(function () {
 
-    $('.carousel').carousel({
-        interval: 5000
-    })
-
     $('#map_canvas').gmap({'zoom':7, 'center':'34.176376,-82.023768', 'scrollwheel': false}).bind('init', function() {
         $('#map_canvas').gmap('addMarker', {'position':'34.176376,-82.023768', 'bounds':false}).click(function() {
             $('#map_canvas').gmap('openInfoWindow', {'content': 'Ninety Six'}, this);
@@ -11,7 +7,4 @@ $(document).ready(function () {
     });
 
 
-
-
 });
-
