2696b70c5dcbf54ebb20d4b123bae959e29fdc06
Trey Robinson <trobinson@captechconsulting.com>
2013-12-18 09:33:51 -0500

    Disabled mouse scrollwheel on map

--- js/site.js
+++ js/site.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         interval: 5000
     })
 
-    $('#map_canvas').gmap({'zoom':7, 'center':'34.176376,-82.023768'}).bind('init', function() {
+    $('#map_canvas').gmap({'zoom':7, 'center':'34.176376,-82.023768', 'scrollwheel': false}).bind('init', function() {
         $('#map_canvas').gmap('addMarker', {'position':'34.176376,-82.023768', 'bounds':false}).click(function() {
             $('#map_canvas').gmap('openInfoWindow', {'content': 'Ninety Six'}, this);
         });
