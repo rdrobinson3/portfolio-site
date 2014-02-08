c8ce792045dff42eb888b8a70f59335dd1a4fb86
Trey Robinson <trobinson@captechconsulting.com>
2014-02-08 07:25:48 -0500

    Cleaned up some warnings from the grunt build

--- app/scripts/contact.js
+++ app/scripts/contact.js
@@ -1,40 +1,41 @@
 /**
  * Created by rrobinson on 12/19/13.
  */
+'use strict';
 $(document).ready(function() {
 
-    var parseAPPID = "X5T1e3ZekYxOuqo7xnOVZDCSApXnV0SwtHoG9VuB";
-    var parseJSID = "ZU2ULmq3h2xZ3Iv3fAoKYZ3ILavf5VfMyly1YdXt";
+    var parseAPPID = 'X5T1e3ZekYxOuqo7xnOVZDCSApXnV0SwtHoG9VuB';
+    var parseJSID = 'ZU2ULmq3h2xZ3Iv3fAoKYZ3ILavf5VfMyly1YdXt';
 
     Parse.initialize(parseAPPID, parseJSID);
-    var CommentObject = Parse.Object.extend("CommentObject");
+    var CommentObject = Parse.Object.extend('CommentObject');
 
-    $("#contactForm").on("submit", function(e) {
+    $('#contactForm').on('submit', function(e) {
         e.preventDefault();
 
-        $("#successBox").addClass("hide");
-        $("#errorBox").addClass("hide");
+        $('#successBox').addClass('hide');
+        $('#errorBox').addClass('hide');
 
-        console.log("Handling the submit");
+        console.log('Handling the submit');
         //add error handling here
         //gather the form data
 
 
         var data = {};
-        data.name = $("#inputName").val();
-        data.email = $("#inputEmail").val();
-        data.comments = $("#inputMessage").val();
+        data.name = $('#inputName').val();
+        data.email = $('#inputEmail').val();
+        data.comments = $('#inputMessage').val();
 
         var comment = new CommentObject();
         comment.save(data, {
             success:function() {
-                console.log("Success");
+                console.log('Success');
                 //Alerts are lame - but quick and easy
-                $("#successBox").slideDown(400).removeClass("hide");
+                $('#successBox').slideDown(400).removeClass('hide');
             },
             error:function(e) {
                 console.dir(e);
-                $("#errorBox").slideDown(400).removeClass("hide");
+                $('#errorBox').slideDown(400).removeClass('hide');
             }
         });
 
